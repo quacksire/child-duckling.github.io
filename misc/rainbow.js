106f07fedc6a6995b6b6bf72518c1055f9ae22f2
child_duckling <19170969+child-duckling@users.noreply.github.com>
2021-03-29 20:56:05 -0700

    push

---
+++ misc/rainbow.js
@@ -0,0 +1,31 @@
+//var body = document.getElementById("rainbow")
+var r;
+var g;
+var b;
+
+while (true) {
+
+
+    if (r >= 200) {
+        r = 0;
+    }
+
+    if (g >= 200) {
+        g = 0;
+    }
+
+    if (b >= 200) {
+        b = 0;
+    }
+
+
+    ////////////////////
+
+    r = r + Math.random() * 10;
+    g = g + Math.random() * 10;
+    b = b + Math.random() * 10;
+
+    ////////////////
+
+    document.getElementById("rainbow").style.backgroundColor('rgb(' + String(r) + ',' + String(g) + ',' + String(b) + ')')
+}
